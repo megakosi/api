eb146c77fe7cb7d10835d32461f4300ed965b019
Kosi Eric <itskosieric@gmail.com>
2021-07-08 20:12:29 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -322,7 +322,7 @@ $clients = [
         [
 
             'owner_name' => 'Lil Bright',
-            'date_purchased' => 'Jun 08 2021  09:26AM',
+            'date_purchased' => 'Jul 08 2021  09:26AM',
             'duration_in_days' => $defaults['duration_in_days'],
             'country_code' => $defaults['country_code'],
             'api_hash' => $defaults['api_hash'],
